bcd7b1a79a3f691c090dcee68cbfd9b8371f71c6
TentakleM1 <tentakelme7@gmail.com>
2024-01-31 16:36:25 +0300

    x

---
+++ front/vite-project/src/pages/auth/auth.ts
@@ -0,0 +1,49 @@
+
+class Registration {
+    constructor(nameInput : string) {
+        this.login = document.getElementById(nameInput);
+        this.password = document.getElementById('password');
+        this.btn = document.getElementById('btn');
+
+        this.loginWord = ''
+        this.passwordWord = ''
+
+        this.login.addEventListener('keyup', e => {
+            if(e.keyCode === 32) {
+                this.login.value = this.loginWord
+              } else {
+                this.loginWord = this.login.value
+              }
+        })
+
+        this.password.addEventListener('keyup', e => {
+            if(e.keyCode === 32) {
+                this.password.value = this.passwordWord
+              } else {
+                this.passwordWord = this.password.value
+              }
+        })
+    }
+
+    getUser() {
+        const user : {login: string, password: string} = {
+            login: this.login.value,
+            password: this.password.value
+        }
+
+        return user
+    }
+
+    getBtnElement() {
+        return this.btn
+    }
+
+}
+
+const registration = new Registration('login')
+
+const btn = registration.getBtnElement()
+
+btn.addEventListener('click', (e) => {
+    console.log(registration.getUser())
+})
